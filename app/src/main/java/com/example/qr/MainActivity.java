32e91f359a088b32ef241c0e9097995f02f7773a
PKS <pkumarsaha71@gmail.com>
2020-02-07 23:49:09 +0600

    Initial commit

---
+++ app/src/main/java/com/example/qr/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.qr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button Generatebutton,Scanbutton;
+    ImageView imageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        Generatebutton = findViewById(R.id.Generatebutton);
+        Scanbutton = findViewById(R.id.Scanbutton);
+        imageView= findViewById(R.id.imageView);
+
+        Generatebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = editText.getText().toString();
+
+                //QRGEncoder
+                // Initializing the QR Encoder with your value to be encoded, type you required and Dimension
+                QRGEncoder qrgEncoder = new QRGEncoder(data, null, QRGContents.Type.TEXT, 10);
+
+                try {
+                    // Getting QR-Code as Bitmap
+                    Bitmap qrbits = qrgEncoder.getBitmap();
+                    // Setting Bitmap to ImageView
+                   // qrImage.setImageBitmap(bitmap);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+}
