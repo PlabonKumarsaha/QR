8e4bae27ad9d4f17cacf8e12763065b10b0f316e
PKS <pkumarsaha71@gmail.com>
2020-02-20 16:03:06 +0600

    up

--- app/src/main/java/com/example/qr/Scanner.java
+++ app/src/main/java/com/example/qr/Scanner.java
@@ -29,6 +29,7 @@ public class Scanner extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
 
